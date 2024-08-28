9978f2e10dca4bec851a0858468e0feb75774571
Philippe Chevreul <philippe.chevreul@cleyrop.com>
2024-08-28 02:39:53 +0200

    feat: test

--- src/app/projects/[projectSlug]/page.tsx
+++ src/app/projects/[projectSlug]/page.tsx
@@ -5,41 +5,11 @@ import { Button } from "@/components/ui/button";
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
 import { Divider } from "@/components/ui/divider";
 import { getPostData } from "@/lib/posts";
+import { routesNames } from "@/lib/utils";
 import { ArrowLeft } from "lucide-react";
 import { Metadata, ResolvingMetadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
-
-export enum routesNames {
-  agile='Agile Lab',
-  agileMotion='AXA Video Agile',
-  ballad='The ballad of the fox',
-  bookstagram='Bookstagram',
-  bright='All the bright places',
-  canaux='Les Canaux',
-  connection='Change Connection',
-  echappoterie='L\'Echappoterie',
-  echappoteriemotion='Vidéo L\'Echappoterie',
-  eclosion='L\'éclosion livresque',
-  finchiscreating='Finch is creating',
-  finchisreading='Finch is reading',
-  flower='A flower in a boot',
-  framework='Management Agile',
-  hope='Hope dies last',
-  inktober='Inktober',
-  jabu='Jabu',
-  key='Key Users',
-  mainsquare='Mainsquare Festival',
-  marsnniversaire='Mars-nniversaire',
-  me='Autoportraits',
-  patriarchy='Dear Patriarchy',
-  personnages='Personnages',
-  portraitsillus='Portraits illustrés',
-  rockin='Rock\'in Evreux',
-  rose='Plus sombre est la rose',
-  sloweare='SloWeAre',
-  sloweareMotion='Vidéo SloWeAre',
-}
 
 type Props = {
   params: { projectSlug: string }
@@ -63,8 +33,6 @@ export async function generateMetadata(
     },
   }
 }
-
-
 
 export default async function Page({ params }: any) {
   const postData = await getPostData(params.projectSlug) as any;
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,34 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export enum routesNames {
+  agile='Agile Lab',
+  agileMotion='AXA Video Agile',
+  ballad='The ballad of the fox',
+  bookstagram='Bookstagram',
+  bright='All the bright places',
+  canaux='Les Canaux',
+  connection='Change Connection',
+  echappoterie='L\'Echappoterie',
+  echappoteriemotion='Vidéo L\'Echappoterie',
+  eclosion='L\'éclosion livresque',
+  finchiscreating='Finch is creating',
+  finchisreading='Finch is reading',
+  flower='A flower in a boot',
+  framework='Management Agile',
+  hope='Hope dies last',
+  inktober='Inktober',
+  jabu='Jabu',
+  key='Key Users',
+  mainsquare='Mainsquare Festival',
+  marsnniversaire='Mars-nniversaire',
+  me='Autoportraits',
+  patriarchy='Dear Patriarchy',
+  personnages='Personnages',
+  portraitsillus='Portraits illustrés',
+  rockin='Rock\'in Evreux',
+  rose='Plus sombre est la rose',
+  sloweare='SloWeAre',
+  sloweareMotion='Vidéo SloWeAre',
+}
